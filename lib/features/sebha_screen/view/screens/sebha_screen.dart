97de86156f444b608d4f12ae25b950c93210a916
last <t.elgebaly1@yahoo.com>
2024-09-26 18:13:58 +0300

    bottom nav bar

---
+++ lib/features/sebha_screen/view/screens/sebha_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HadithScreen extends StatelessWidget {
+  const HadithScreen({super.key});
+
+  static const hadithScreen = 'hadithScreen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
